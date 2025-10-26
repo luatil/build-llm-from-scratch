a90773f927c4d0e21f809ab5e9ba9e28b10ccc24
Lua Nowacki Scavacini Santilli <luasantilli@gmail.com>
2025-10-26 14:29:31 -0300

    Create GPTDatasetV1

---
+++ gpt_dataset_v1.py
@@ -0,0 +1,75 @@
+import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
+from typing import Protocol, Sequence
+
+
+class Tokenizer(Protocol):
+    def encode(self, text: str) -> list[int]:
+        raise NotImplementedError
+
+    def decode(self, tokens: Sequence[int], errors="replace") -> str:
+        raise NotImplementedError
+
+
+class GPTDatasetV1(Dataset):
+    def __init__(
+        self, txt: str, tokenizer: Tokenizer, max_length: int, stride: int
+    ) -> None:
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i : i + max_length]
+            target_chunk = token_ids[i + 1 : i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self) -> int:
+        return len(self.input_ids)
+
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader_v1(
+    txt: str,
+    batch_size: int = 4,
+    max_length: int = 256,
+    stride: int = 128,
+    shuffle: bool = True,
+    drop_last: bool = True,
+    num_workers: bool = True,
+) -> DataLoader:
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+    return dataloader
+
+
+if __name__ == "__main__":
+    with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
+    )
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+    print(f"{first_batch=}")
+    second_batch = next(data_iter)
+    print(f"{second_batch=}")
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=8, max_length=4, stride=4, shuffle=False
+    )
+    data_iter = iter(dataloader)
+    inputs, targets = next(data_iter)
+    print(f"{inputs=}")
+    print(f"{targets=}")
